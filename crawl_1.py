485cedc9c5ae5ffb9b0d811e3edbe95e0c9f2661
王俊斌 <JinBin@AppledeMacBook-Pro.local>
2023-10-30 22:02:00 +0800

    10/30

---
+++ crawl_1.py
@@ -0,0 +1,59 @@
+#抓K線
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+
+file_path = 'companys.txt'  
+
+
+with open(file_path, 'r') as file:
+    lines = file.readlines()  
+
+
+for i in range(0, len(lines), 2):
+    
+    line1 = lines[i].strip()
+    line2 = lines[i + 1].strip()
+    
+    download_folder = f'/Users/JinBin/Desktop/大學課程/大三上/機器學習/final_project/company_info/{line1}/K'  # 请将路径替换为实际文件夹路径
+    os.makedirs(download_folder,exist_ok=True)
+    
+    options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory': download_folder}
+    options.add_experimental_option('prefs', prefs)
+
+    
+    driver = webdriver.Chrome(options=options)
+
+    
+    driver.get(f"https://goodinfo.tw/tw/ShowK_Chart.asp?STOCK_ID={line2}")
+
+    
+    wait = WebDriverWait(driver, 10)  
+    download_button = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='匯出XLS']")))
+
+    
+    select = Select(wait.until(EC.presence_of_element_located((By.ID, "selK_ChartPeriod"))))
+
+    
+    select.select_by_value("365")  # 请根据实际选项的值进行替换
+
+    
+    wait.until(EC.staleness_of(download_button))
+
+
+    download_button = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='匯出XLS']")))
+
+
+    download_button.click()
+
+    while not os.path.exists(os.path.join(download_folder, 'K_Chart.xls')):
+        time.sleep(1)  
+
+    driver.quit()
+    time.sleep(10)
